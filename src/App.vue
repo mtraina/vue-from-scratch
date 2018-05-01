a704cd254a4f8d61dd0075ade6e8de246a7e496c
mtraina <matteo.traina.mail@gmail.com>
2018-05-01 10:16:17 +0200

    sample project created

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+   <h1>Hello from the App</h1>
+</template>
+
+<script>
+   export default {
+       name: 'App'
+   }
+</script>
---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+import Vue from 'vue'
+import App from './App'
+
+/* eslint-disable-next-line no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+}).$mount('#app')
